0a65678d50fc241ae5ccbf5542315d833efab151
Enrico van Oosten <e.m.p.v.oosten@hotmail.com>
2013-09-05 01:42:10 +0200

    shadowz start

--- sdl2-3d/sdl2-3d/EntityFactory.cpp
+++
@@ -1,8 +0,0 @@
-#include "EntityFactory.h"
-
-coment::Entity& EntityFactory::createPlayer(coment::World& world, float x, float y, float z)
-{
-	EntityBuilder b(world, world.createEntity());
-
-	return b.position(x, y, z).velocity(0, 0, 0).getEntity();
-}
