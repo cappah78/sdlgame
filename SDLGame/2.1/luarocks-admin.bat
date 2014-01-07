50de5a70bb038521b12eb1f1573a0690c8b9351d
Enrico van Oosten <e.m.p.v.oosten@hotmail.com>
2014-01-07 23:56:31 +0100

    just added msvcr100d dll's...

--- SDLGame/2.1/luarocks-admin.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SETLOCAL
-SET LUA_PATH=C:\Users\Enrico\Documents\GitHub\sdlgame\sdl2-3d\2.1\lua\?.lua;C:\Users\Enrico\Documents\GitHub\sdlgame\sdl2-3d\2.1\lua\?\init.lua;%LUA_PATH%
-IF NOT "%LUA_PATH_5_2%"=="" (
-   SET LUA_PATH_5_2=C:\Users\Enrico\Documents\GitHub\sdlgame\sdl2-3d\2.1\lua\?.lua;C:\Users\Enrico\Documents\GitHub\sdlgame\sdl2-3d\2.1\lua\?\init.lua;%LUA_PATH_5_2%
-)
-SET PATH=C:\Users\Enrico\Documents\GitHub\sdlgame\sdl2-3d\2.1\;%PATH%
-"lua5.1" "C:\Users\Enrico\Documents\GitHub\sdlgame\sdl2-3d\2.1\luarocks-admin.lua" %*
-ENDLOCAL
--- SDLGame/SDLGame/Engine/Graphics/Graphics.cpp
+++
@@ -1,5 +0,0 @@
-#include "Graphics.h"
-
-SDL_Window* Graphics::m_window;
-unsigned int Graphics::m_screenWidth;
-unsigned int Graphics::m_screenHeight;
