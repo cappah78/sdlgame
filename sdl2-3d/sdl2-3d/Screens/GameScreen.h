d668dcdb6890e7c781161088660b955bf9026333
Enrico van Oosten <e.m.p.v.oosten@hotmail.com>
2014-02-07 13:50:57 +0100

    Update GameScreen.h
    D3D testing things..

--- sdl2-3d/sdl2-3d/Screens/GameScreen.h
+++ sdl2-3d/sdl2-3d/Screens/GameScreen.h
@@ -1,20 +1,22 @@
 #ifndef GAME_SCREEN_H_
 #define GAME_SCREEN_H_
 
-#include "..\Engine\Model\IScreen.h"
-#include "..\Engine\Model\IMouseListener.h"
-#include "..\Engine\Model\IKeyListener.h"
+#include "../Engine/Model/IScreen.h"
+#include "../Engine/Model/IMouseListener.h"
+#include "../Engine/Model/IKeyListener.h"
 
-#include "..\Engine\Graphics\Renderers\SkyBox.h"
-#include "..\Engine\Graphics\PerspectiveCamera.h"
-#include "..\Engine\Utils\FPSCameraController.h"
+#include "../Engine/Graphics/PerspectiveCamera.h"
+#include "../Engine/Utils/FPSCameraController.h"
 
-#include "..\Voxel\VoxelRenderer.h"
+#include "../Voxel/VoxelRenderer.h"
 #include "../Voxel/VoxelWorld.h"
 #include "../Voxel/WorldRenderer.h"
 #include "../Voxel/Deferred/DeferredWorldRenderer.h"
 
-
+struct IDXGISwapChain;
+struct ID3D11Device;
+struct ID3D11DeviceContext;
+struct ID3D11RenderTargetView;
 class VoxelCache;
 class TextureArray;
 
@@ -28,18 +30,28 @@ public:
 	void resize(int width, int height);
 	virtual bool keyDown(SDL_Keysym key) override;
 	virtual bool keyUp(SDL_Keysym key) override;
+
 private:
-	//SkyBox m_skyBox;
+	//tmp d3d stuffs
+	enum RenderMode { OPENGL, D3D };
+	void initializeD3D();	//tmp testing stuffs
+	void disposeD3D();
+	IDXGISwapChain* m_swapChain;
+	ID3D11Device* m_device;
+	ID3D11DeviceContext* m_deviceContext;
+	ID3D11RenderTargetView* m_backBuffer;
+	void renderOpenGL(float deltaSec);
+	void renderD3D(float deltaSec);
+	RenderMode m_renderMode;
+	///
+
 	FPSCameraController m_cameraController;
 	PerspectiveCamera m_camera;
-
 	Shader m_modelShader;
 	Mesh m_mesh;
-
 	TextureManager m_textureManager;
 	VoxelWorld m_world;
 	WorldRenderer m_worldRenderer;
-	DeferredWorldRenderer m_deferredWorldRenderer;
 	VoxelRenderer m_voxelRenderer;
 	TextureArray* m_tileSet;
 };
